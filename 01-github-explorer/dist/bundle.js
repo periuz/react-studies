cb098c81f658c67ed1da92a847d2e9c47818644c
thiago-perius <thiagoperiussilva@gmail.com>
2025-06-11 17:30:35 -0300

    new: add bundle from second class of rocketseat course

--- 01-github-explorer/dist/bundle.js
+++ 01-github-explorer/dist/bundle.js
@@ -1,8 +1,9 @@
-// This code demonstrates the use of optional chaining in JavaScript.
-// Optional chaining allows you to safely access deeply nested properties without having to check each level for null or undefined.
-// If a property is null or undefined, it will return undefined instead of throwing an error.
+"use strict";
 
-const user = {
-  name: 'Thiago'
-};
-console.log(user?.name); // Thiago
+var _react = _interopRequireDefault(require("react"));
+var _reactDom = _interopRequireDefault(require("react-dom"));
+function _interopRequireDefault(e) { return e && e.__esModule ? e : { "default": e }; }
+function App() {
+  return /*#__PURE__*/_react["default"].createElement("h1", null, "Hello World");
+}
+_reactDom["default"].render(/*#__PURE__*/_react["default"].createElement(App, null), document.getElementById('root'));
